c41673caf48852e804f8d665741dbb78ba977a65
Renato Marino Henz <renatomarinohenz@gmail.com>
2023-12-06 18:27:01 -0300

    feat: created tags deleting API

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -34,6 +34,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
 
 class TagViewSet(
+    mixins.DestroyModelMixin,
     mixins.UpdateModelMixin,
     mixins.ListModelMixin,
     viewsets.GenericViewSet,
